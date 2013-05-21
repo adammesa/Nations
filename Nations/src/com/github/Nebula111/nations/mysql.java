bdb30bbb8d2f58de85d4383cf7ede8a026712f3e
Colin <epicshadow111@gmail.com>
2013-05-20 20:20:37 -0600

    More mysql work... I mean attempts..

--- Nations/src/com/github/Nebula111/nations/mysql.java
+++ Nations/src/com/github/Nebula111/nations/mysql.java
@@ -1,5 +1,10 @@
 package com.github.Nebula111.nations;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 
 public class mysql { 
@@ -13,4 +18,13 @@ public class mysql {
 	String sqlusername = plugin.getConfig().getString("username");
 	String sqlpwd = plugin.getConfig().getString("password");
 	String sqlurl = plugin.getConfig().getString("host");
+	
+	public void createTables() throws SQLException {
+	try {	Connection conn = DriverManager.getConnection(plugin.getConfig().getString("host"), plugin.getConfig().getString("username"), plugin.getConfig().getString("password"));
+		PreparedStatement QueryStatement = conn.prepareStatement("uwguifgiuwgfuigwuifgw");
+		QueryStatement.executeUpdate();
+		QueryStatement.close();
+		conn.close(); 
+		} catch(Exception err){}		//So confused guys...
+	}
 }
